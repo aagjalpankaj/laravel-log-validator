92accf1a96b386d901438ecc175b5b7320e68b40
Pankaj Aagjal <aagjalpankaj@gmail.com>
2025-04-06 14:37:08 +0200

    publish config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,4 +4,21 @@ declare(strict_types=1);
 
 namespace Aagjalpankaj\LaravelLogValidator;
 
-final class ServiceProvider extends \Illuminate\Support\ServiceProvider {}
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+final class ServiceProvider extends BaseServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/laravel-log-validator.php' => config_path('laravel-log-validator.php'),
+        ], 'laravel-log-validator-config');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-log-validator.php', 'laravel-log-validator'
+        );
+    }
+}
